f7731d15584dd6cbbcc0803b35ce2f56c566ffe3
Georgii_Vysokovksyi <159915255+Georgii-Smirnov@users.noreply.github.com>
2024-07-15 22:27:09 +0400

    add template

---
+++ lesson-76/main.js
@@ -0,0 +1,10 @@
+const firstName = 'Georgii' 
+const lastName = 'Smirnov'
+const userLanguage = 'Russian'
+let age = '18'
+const usersProffesion = 'Web-Developer'
+
+console.log(firstName, lastName)
+console.log( age, 'age',)
+console.log( userLanguage, 'Language')
+console.log( usersProffesion)
