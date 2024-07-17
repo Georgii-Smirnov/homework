3fbd80174b660d250e14984348839c91fc69548d
Georgii_Vysokovksyi <159915255+Georgii-Smirnov@users.noreply.github.com>
2024-07-18 00:02:44 +0400

    add template

---
+++ lesson-81-homework/main.js
@@ -0,0 +1,11 @@
+const numbers = [1, 6, 11, 10, 55, 54, 22, 2, 18, 99, 16]
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0)
+    console.log(numbers[i])
+}
+
+const rainbow = ['red', 'orange', 'yellow', 'green', 'indigo','blue','violet']
+for (let i = rainbow.length - 1; i >= 0; i--) {
+console.log(rainbow[i])
+}
+
