abe62d25561d053f4993089e98def368ad6fcd9e
Georgii_Vysokovksyi <159915255+Georgii-Smirnov@users.noreply.github.com>
2024-11-13 00:37:06 +0400

    Add files via upload

--- rise-creative/js/main.js
+++ rise-creative/js/main.js
@@ -18,4 +18,78 @@
     }
   }
 
+  document.addEventListener('DOMContentLoaded', () => {
+    const passwordInputs = document.querySelectorAll('input[type="password"]');
+    const errorMessage = document.querySelector('#errorMessage');
+
+    // Открытие и закрытие модальных окон
+    document.addEventListener('click', (e) => {
+      const target = e.target;
+
+      // Обработка открытия модальных окон
+      if (target.matches('#openModalBtn, #openModalBtn-in')) {
+        e.preventDefault();
+        const modalId = target.id === 'openModalBtn' ? '#modal1' : '#modal2';
+        document.querySelector(modalId).classList.add('body--opened-modal-up');
+      }
+
+      // Обработка закрытия модальных окон
+      if (target.closest('.modal__close-btn') || target.classList.contains('modal__in')) {
+        e.preventDefault();
+        target.closest('.modal__in').classList.remove('body--opened-modal-up');
+      }
+    });
+
+    // Проверка совпадения паролей
+    const checkPasswords = () => {
+      errorMessage.style.display = passwordInputs[0].value !== passwordInputs[1].value ? 'block' : 'none';
+    };
+
+    passwordInputs.forEach(input => input.addEventListener('input', checkPasswords));
+
+    // Функция показа/скрытия пароля
+    const togglePassword = (input) => {
+      input.type = input.type === 'password' ? 'text' : 'password';
+    };
+
+    // Присоединение обработчиков событий для кнопок
+    document.querySelectorAll('.password-toggle').forEach(toggle => {
+      toggle.addEventListener('click', () => {
+        const input = toggle.closest('.input-container').querySelector('input[type="password"], input[type="text"]');
+        togglePassword(input);
+      });
+    });
+  });
+
+  // Свайпер
+
+  const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2.38,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.content-gallery__next',
+      prevEl: '.content-gallery__prev',
+    },
+    breakpoints: {
+      801: {
+        slidesPerView: 2.38,
+        spaceBetween: 30,
+      },
+      450: { // Настройки для ширины от 800px и ниже
+        slidesPerView: 2,
+        spaceBetween: 25,
+      },
+      350: { // Настройки для ширины от 800px и ниже
+        slidesPerView: 1,
+        spaceBetween: 15,
+      },
+    },
+  });
+
 })();
+
+
+
+
+
+
